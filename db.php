48d7e661ae283be238add37a08724b01cfc626bc
Lahirusampath2000 <117251293+Lahirusampath2000@users.noreply.github.com>
2023-07-18 19:42:52 +0530

    Add files via upload

---
+++ db.php
@@ -0,0 +1,20 @@
+<?php
+$dbhost = 'localhost';
+$dbuser = 'root';
+$dbpass = '';
+$dbname = '';
+
+//create a DB connection 
+$conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+//if the DB connection fails, display an error message and exit
+if (!$conn)
+{
+ die('Could not connect: ' . mysqli_error($conn));
+}
+else{
+//select the database
+mysqli_select_db($conn, $dbname);	
+echo("connection established");	
+}
+
+?>
